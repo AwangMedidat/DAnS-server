7af7937cd836bc7d12cb4411e5974aa33fe4144f
Awang <awangmedidat98@gmail.com>
2023-07-14 23:32:42 +0700

    Add cors

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ const cors = require("cors");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.use("/api", router);
-
 
 app.listen(port, () => {
   console.log(`connection is live at port ${port}`);
